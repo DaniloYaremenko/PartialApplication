56634181651c8338f54ae406b4b4851a7732e96b
DanilYaremenko <danilyaremenko@gmail.com>
2022-01-03 20:21:20 +0200

    Update

--- Exercises/3-lambda.js
+++ Exercises/3-lambda.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const tagged = (pref, str) => `[${pref}] ${str}`;
-
-// Define function tagDate that prepends current date to the string.
-// E.g. tagDate('My String') === '[2019-11-14] My String'
-// Use function tagged to implement tagDate.
-
-const tagDate = null;
-
+const tagDate = (string) => {
+  const date = new Date();
+  const beautifulDate = date.toISOString();
+  return beautifulDate + ' ' + string;
+};
 module.exports = { tagDate };
+console.log(tagDate('da'))
--- Exercises/5-curry.js
+++ Exercises/5-curry.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// Check 4 digit pin.
 const EXPECTED_PIN = '4967';
 const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 
-// Define function curry that accepts the length of the function
-// (amount of function arguments) and the function.
-
-const curry = (length, fn) => (...args) => null;
-
-// Allows to enter pin code by one character,
-// Usage: press('3')('4')('5')('6')
+const curry = (length, fn) => (...args) => {
+  if (length !== 1) {
+    const func = fn.bind(null, ...args);
+    return curry(length - 1, func);
+  } else {
+      return fn(...args)
+  }
+};
 const press = curry(4, checkPin);
 
 module.exports = { press };
